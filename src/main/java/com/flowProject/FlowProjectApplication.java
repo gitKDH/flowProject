0f8c4866ebc051e1ddd5d0ec150e46d8d15bf906
gitKDH <reg0212@naver.com>
2024-01-30 15:49:02 +0900

    [DB] mysql 정보

---
+++ src/main/java/com/flowProject/FlowProjectApplication.java
@@ -0,0 +1,13 @@
+package com.flowProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowProject/FlowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
