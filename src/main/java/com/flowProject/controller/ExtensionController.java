ceba524a69301fb1ec95b26c813929142f433388
gitKDH <reg0212@naver.com>
2024-02-02 22:23:00 +0900

    Remove sensitive information from application.properties

--- src/main/java/com/flowProject/controller/ExtensionController.java
+++ src/main/java/com/flowProject/controller/ExtensionController.java
@@ -5,10 +5,7 @@ import com.flowProject.service.FileUploadService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
